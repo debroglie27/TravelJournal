79f795fa12ec3c895b4436cc6fe8048b94c24591
Arijeet De <arijeetde@gmail.com>
2023-12-26 12:05:19 +0530

    creating the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
+import Card from './components/Card'
+import data from './data'
 import './App.css'
 
 function App() {
   return (
     <>
+      <Navbar />
     </>
   )
 }
